01495f7f4b00d55264d6db0eef6ccd76fbb68947
Nadja Sondag <nsondag@w-r8-p3.s19.be>
2019-01-04 16:12:00 +0100

    parsing tubes

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: hvromman <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/19 11:36:12 by hvromman          #+#    #+#              #
-#    Updated: 2018/12/21 16:59:21 by nallani          ###   ########.fr        #
+#    Updated: 2019/01/04 15:59:40 by nsondag          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,9 +17,9 @@ CFLAGS = -Wall -Wextra -Werror
 SRCS = srcs/
 HEAD = includes/
 LIB = libft/
-FILE = lem_in.h
+FILE = lem-in.h
 H = $(addprefix $(HEAD), $(FILE))
-FILES = 
+FILES = parser.o
 OBJ = $(addprefix $(SRCS), $(FILES))
 SANITIZE = -fsanitize=address
 
--- includes/lem-in.h
+++ includes/lem-in.h
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lem_in.h                                           :+:      :+:    :+:   */
+/*   lem-in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hvromman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 14:15:16 by hvromman          #+#    #+#             */
-/*   Updated: 2019/01/04 14:15:18 by hvromman         ###   ########.fr       */
+/*   Updated: 2019/01/04 16:10:57 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
-# include "libft.h"
+#include "libft.h"
+#include <stdlib.h>
+
+typedef	struct	s_adj
+{
+	int		index;
+	char	*name;
+	int		*tab;
+}				t_adj;
 
 typedef struct	s_a
 {
@@ -21,6 +29,7 @@ typedef struct	s_a
 	int		tab_size;
 	char	*start;
 	char	*end;
+	t_adj	*adj;
 }				t_a;
 
 #endif
