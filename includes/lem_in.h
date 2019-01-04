65249f0e33892715c6e3e9afbe25026e8fa00833
Nadja Sondag <nsondag@w-r8-p3.s19.be>
2019-01-04 14:51:47 +0100

    struct t_adj

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,12 +6,14 @@
 /*   By: hvromman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 14:15:16 by hvromman          #+#    #+#             */
-/*   Updated: 2019/01/04 14:15:18 by hvromman         ###   ########.fr       */
+/*   Updated: 2019/01/04 14:50:47 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
+
+#include "libft.h"
 
 typedef struct	s_a
 {
@@ -19,6 +21,14 @@ typedef struct	s_a
 	int		tab_size;
 	char	*start;
 	char	*end;
+	t_adj	adj;
 }				t_a;
+
+typedef	struct	s_adj
+{
+	int		index;
+	char	*name;
+	int		*tab;
+}				t_adj;
 
 #endif
