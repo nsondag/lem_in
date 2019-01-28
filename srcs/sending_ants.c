034d9d97ac777ba857df4fdae7ab8c5cdcb354be
Nadja Sondag <nsondag@w-r2-p3.s19.be>
2019-01-28 15:17:28 +0100

    sending 1 path: wrong ending condition

--- srcs/sending_ants.c
+++ srcs/sending_ants.c
@@ -6,7 +6,7 @@
 /*   By: nsondag <nsondag@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 14:49:18 by nsondag           #+#    #+#             */
-/*   Updated: 2019/01/27 21:10:12 by nsondag          ###   ########.fr       */
+/*   Updated: 2019/01/28 15:16:32 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int sending_ants(t_a *ant)
 {
 	int i;
 	int j;
+	int k;
 	int	*start_ant;
 	int	nb_move;
 
@@ -28,8 +29,25 @@ int sending_ants(t_a *ant)
 		start_ant[i] = ant->nb_ant_per_path[i - 1] + 1;
 	//---------------------------------------------------------------------
 	nb_move = 1;
-	j = -1;
-		ft_printf("L%d-%d ", start_ant[j] + i, ant->path[i][j]);
-	ft_printf("\n");
+	i = 0;
+	k = 0;
+	while (ant->nb_ant)
+	{
+		j = 0;
+		k = k - nb_move + 1;
+		while (j < nb_move)
+		{
+			ft_printf("L%d-%d ", start_ant[i] + k,
+					ant->path[i][nb_move - j - 1]);
+			j++;
+			k++;
+		}
+		ft_printf("\n");
+		if (nb_move < ant->len_path[i])
+			nb_move++;
+		if (ant->nb_ant < ant->len_path[i])
+			nb_move--;
+		ant->nb_ant--;
+	}
 	return (0);
 }
