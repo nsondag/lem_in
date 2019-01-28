76db5813684535a6e4adcca518fe5be49cfce1f0
Nadja Sondag <nsondag@e-r11-p2.s19.be>
2019-01-28 16:44:08 +0100

    work in progress

--- srcs/sending_ants.c
+++ srcs/sending_ants.c
@@ -6,7 +6,7 @@
 /*   By: nsondag <nsondag@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 14:49:18 by nsondag           #+#    #+#             */
-/*   Updated: 2019/01/28 15:16:32 by nsondag          ###   ########.fr       */
+/*   Updated: 2019/01/28 16:43:48 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,25 @@ int sending_ants(t_a *ant)
 	nb_move = 1;
 	i = 0;
 	k = 0;
-	while (ant->nb_ant)
+	while (ant->nb_ant + ant->len_path[i] > 0)
 	{
 		j = 0;
 		k = k - nb_move + 1;
 		while (j < nb_move)
 		{
-			ft_printf("L%d-%d ", start_ant[i] + k,
+			ft_printf("L%d-%d", start_ant[i] + k,
 					ant->path[i][nb_move - j - 1]);
 			j++;
+			(j < nb_move) ? ft_printf(" ") : ft_printf("\n");
 			k++;
 		}
-		ft_printf("\n");
 		if (nb_move < ant->len_path[i])
 			nb_move++;
-		if (ant->nb_ant < ant->len_path[i])
-			nb_move--;
-		ant->nb_ant--;
+		//if (ant->nb_ant < ant->len_path[i]--)
+		//	nb_move--;
+		//ft_printf("%d %d\n", nb_move, ant->len_path[i]);
+		if (nb_move == ant->len_path[i])
+			ant->nb_ant--;
 	}
 	return (0);
 }
