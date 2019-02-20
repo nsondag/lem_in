ac87fdfd0f52cf3d7ec35ec9777f98704a4a718c
Nadja Sondag <nsondag@e-r5-p2.s19.be>
2019-02-20 15:44:05 +0100

    work in progress

--- srcs/crossing_path.c
+++ srcs/crossing_path.c
@@ -6,7 +6,7 @@
 /*   By: nsondag <nsondag@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/16 15:58:39 by nsondag           #+#    #+#             */
-/*   Updated: 2019/02/20 14:47:31 by nsondag          ###   ########.fr       */
+/*   Updated: 2019/02/20 15:43:35 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 int crossing_path(t_path ***path, int f)
 {
+	int i;
+	int j;
 	int p1;
 	int p2;
 	int cross;
 	int count;
 	int	index1;
 	int index2;
-	t_path **path1;
-	t_path **path2;
+	t_path *path1;
+	t_path *path2;
 
 	cross = -1;
 	p1 = 1;
@@ -48,19 +50,49 @@ int crossing_path(t_path ***path, int f)
 		p2++;
 		count++;
 	}
-	ft_printf("i1 %d i2 %d\n", index1, index2);
-	ft_printf("count %d\n", count);
+	if (count == 1)
+		return (INVALID);
+	ft_printf("------------new_path----------------\n");
+	//ft_printf("i1 %d i2 %d\n", index1, index2);
+	//ft_printf("count %d\n", count);
 	int size1;
 	int size2;
 	size1 = index1 - index2 + path[f][1]->len_path - (2 * count) + 2;
 	size2 = index2 - index1 + path[f][0]->len_path;
-	ft_printf("size1 %d\n", size1);
-	ft_printf("size2 %d\n", size2);
-	if (!(path1 = malloc(sizeof(t_path**) * size1)))
+	//ft_printf("size1 %d\n", size1);
+	//ft_printf("size2 %d\n", size2);
+	if (!(path1 = malloc(sizeof(t_path))))
 		return (MERROR);
-	if (!(path2 = malloc(sizeof(t_path**) * size2)))
+	if (!(path2 = malloc(sizeof(t_path))))
 		return (MERROR);
-	if (count == 1)
-		return (INVALID);
+	if (!(path1->chain = malloc(sizeof(int) * size1)))
+		return (MERROR);
+	if (!(path2->chain = malloc(sizeof(int) * size2)))
+		return (MERROR);
+	i = -1;
+	while (++i < index1)
+	{ 
+		path1->chain[i] = path[f][0]->chain[i];
+		ft_printf("i %d: %d\n", i, path1->chain[i]);
+	}
+	j = 0;
+	i--;
+	while (++i < size1)
+		path1->chain[i] = path[f][1]->chain[index2 + j++ + count];
+	i = -1;
+	while (++i < index2)
+		path2->chain[i] = path[f][1]->chain[i];
+	j = 0;
+	i--;
+	while (++i < size2)
+		path2->chain[i] = path[f][0]->chain[index1 + j++];
+	//print new paths
+	i = 0;
+	while (i < size1)
+		ft_printf("--- %d\n", path1->chain[i++]);
+	i = 0;
+	ft_printf("\n");
+	while (i < size2)
+		ft_printf("--- %d\n", path2->chain[i++]);
 	return (VALID);
 }
