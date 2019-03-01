11e2280977033e486216f0b5d5379bc1e0023925
Hubert Vromman <hvromman@e-r5-p6.s19.be>
2019-03-01 13:47:44 +0100

    timer

--- srcs/main.c
+++ srcs/main.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "lem_in.h"
+# include <time.h>
 
 int		find(t_a *all)
 {
@@ -49,11 +50,16 @@ int		main(void)
 	int		ret;
 
 	ft_bzero(&ant, sizeof(ant));
+	clock_t e = clock();
 	read_room(&ant);
 	if (!(ant.room) || !(ant.room + 1) || !(ant.buf))
 		exit_func(INVALID, &ant);
+	clock_t f = clock();
+	ft_printf("read_room %f\n%>", (f-e) / (double) CLOCKS_PER_SEC, 2);
 	if ((ret = parse(&ant)))
 		exit_func(ret, &ant);
+	clock_t g = clock();
+	ft_printf("parse %f\n%>", (g-f) / (double) CLOCKS_PER_SEC, 2);
 	search_for_deadend(ant.room, ant.nb_room);
 	search_for_mult_path(&ant, 2);
 	ant.start_room = 0;
@@ -62,7 +68,10 @@ int		main(void)
 	modify_tubes_first(&ant);
 	ant.path = malloc(sizeof(t_path**) * ant.room[ant.start_room].nb_tubes);
 	ant.nb_move = ft_memalloc(sizeof(int) * ant.room[ant.start_room].nb_tubes);
+	clock_t u = clock();
 	find(&ant);
+	clock_t v = clock();
+	ft_printf("find %f\n%>", (v-u) / (double) CLOCKS_PER_SEC, 2);
 	calculate_start(&ant, ant.path[ant.nb_used], ant.nb_used + 1);
 	ft_printf("-----\n%s\n-----\n", ant.data);
 	print_sol(&ant, ant.path[ant.nb_used], ant.nb_used);
