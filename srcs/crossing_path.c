55c2874d6c4b0c4c8374a5b82b2d145ac9681f71
Luna <luna@Tchuuk.local>
2019-03-03 14:34:38 +0100

    nip crossing

--- srcs/crossing_path.c
+++ srcs/crossing_path.c
@@ -12,82 +12,102 @@
 
 #include "lem_in.h"
 
+int		*find_crossing(t_path **path)
+{	
+		int *i;
+
+		if (!(i = (int*)malloc(sizeof(int) * 2)))
+				return(NULL);
+		i[0] = 1;
+		while (path[0]->chain[i[0]] != 1)
+		{
+				i[1] = path[1]->len_path - 2;
+				while (i[1])
+				{
+						if (path[0]->chain[i[0]] == path[1]->chain[i[1]])
+								break ;	
+						i[1]--;
+				}
+				i[0]++;
+		}
+		i[0]--;
+		return (i);
+}
+
+int count_crossing_len(t_path **path, int i1, int i2)
+{
+		int count;
+
+		count = 0;
+		while (path[0]->chain[i1] == path[1]->chain[i2])
+		{
+				i1--;
+				i2++;
+				count++;
+		}
+		return (count);
+}
+
+int *copy_path1(t_path **path, int count, int *index)
+{
+		int i;
+		int j;
+		int	*new_path;
+
+		if (!(new_path = malloc(sizeof(int) * path[0]->len_path)))
+				return (NULL);
+		i = -1;
+		while (++i < index[0] - count + 2)
+				new_path[i] = path[0]->chain[i];
+		j = 0;
+		while (i < path[0]->len_path)
+				new_path[i++] = path[1]->chain[index[1] + j++ + count];
+		free(path[0]->chain);
+		return (new_path);
+}
+
+int *copy_path2(t_path **path, int *index)
+{
+		int i;
+		int j;
+		int	*new_path;
+
+		if (!(new_path = malloc(sizeof(int) * path[1]->len_path)))
+				return (NULL);
+		i = -1;
+		while (++i < index[1])
+				new_path[i] = path[1]->chain[i];
+		j = 0;
+		while (i < path[1]->len_path)
+				new_path[i++] = path[0]->chain[index[0] + j++];
+		free(path[1]->chain);
+		return (new_path);
+}
+
 int crossing_path(t_path ***path, int f)
 {
-	int i;
-	int j;
-	int p1;
-	int p2;
-	int count;
-	int	index1;
-	int index2;
-	int *path1;
-	int *path2;
-	int	size1;
-	int	size2;
+		int *index;
+		int count;
+		int *path1;
+		int *path2;
+		int tmp;
 
-	p1 = 1;
-	while (path[f][0]->chain[p1] != 1)
-	{
-		p2 = path[f][1]->len_path - 2;
-		while (p2)
+		index = find_crossing(path[f]);
+		count = count_crossing_len(path[f], index[0], index[1]);
+		if (count > 1)
 		{
-			if (path[f][0]->chain[p1] == path[f][1]->chain[p2])
-				break ;	
-			p2--;
+			tmp = path[f][0]->len_path;
+			path[f][0]->len_path = index[0] - index[1] +
+				path[f][1]->len_path - (2 * count) + 2;
+			path[f][1]->len_path = index[1] - index[0] + tmp;
+		path1 = copy_path1(path[f], count, index);
+		path2 = copy_path2(path[f], index);
+		if (!(path[f][0]->chain = malloc(sizeof(int) * path[f][0]->len_path)))
+				return (MERROR);
+		if (!(path[f][1]->chain = malloc(sizeof(int) * path[f][1]->len_path)))
+				return (MERROR);
+		ft_memcpy(path[f][0]->chain, path1, 4 * path[f][0]->len_path);	
+		ft_memcpy(path[f][1]->chain, path2, 4 * path[f][1]->len_path);	
 		}
-		p1++;
-	}
-	p1--;
-	index1 = p1;
-	index2 = p2;
-	count = 0;
-	while (path[f][0]->chain[p1] == path[f][1]->chain[p2])
-	{
-		p1--;
-		p2++;
-		count++;
-	}
-	if (count == 1)
-		return (INVALID);
-	if (count == 0)
-		return (VALID);
-	ft_printf("------------new_path----------------\n");
-	size1 = index1 - index2 + path[f][1]->len_path - (2 * count) + 2;
-	size2 = index2 - index1 + path[f][0]->len_path;
-	if (!(path1 = malloc(sizeof(int) * size1)))
-		return (MERROR);
-	if (!(path2 = malloc(sizeof(int) * size2)))
-		return (MERROR);
-	i = -1;
-	while (++i < index1 - count + 2)
-		path1[i] = path[f][0]->chain[i];
-	j = 0;
-	while (i < size1)
-		path1[i++] = path[f][1]->chain[index2 + j++ + count];
-	i = -1;
-	while (++i < index2)
-		path2[i] = path[f][1]->chain[i];
-	j = 0;
-	while (i < size2)
-		path2[i++] = path[f][0]->chain[index1 + j++];
-	path[f][0]->len_path = size1;
-	path[f][1]->len_path = size2;
-	free(path[f][0]->chain);
-	free(path[f][1]->chain);
-	if (!(path[f][0]->chain = malloc(sizeof(int) * size1)))
-		return (MERROR);
-	if (!(path[f][1]->chain = malloc(sizeof(int) * size2)))
-		return (MERROR);
-	ft_memcpy(path[f][0]->chain, path1, 4 * size1);	
-	ft_memcpy(path[f][1]->chain, path2, 4 * size2);	
-	//print new paths
-	i = 0;
-	while (i < size1)
-		ft_printf("--- %d\n", path[f][0]->chain[i++]);
-	i = 0;
-	ft_printf("\n");
-	while (i < size2)
-		ft_printf("--- %d\n", path[f][1]->chain[i++]);
-	return (VALID);
+		return (count == 1 ? INVALID : VALID);
 }
