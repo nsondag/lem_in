608931cec516fef1525ab7c50b3f8bfa359c4b8d
Hubert Vromman <hvromman@w-r8-p2.s19.be>
2019-03-17 14:57:33 +0100

    exec du turfu

--- exec.sh
+++ exec.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 c=0
 r=0
-for i in {1..100}; do
-./generator --big-superposition | ./lem-in > test.txt
+h=0
+for ((i=1; i <= $1; i++)); do
+./generator --big > b$i
+cat b$i | ./lem-in > f$i
+if ((`echo $?` != 0)); then
+exit
+fi
 printf "%-9s: " "expected"
-e=`cat test.txt | grep -m 1 "required:" | grep -o "[0-9][0-9]*"`
+e=`cat f$i | grep -m 1 "required:" | grep -o "[0-9][0-9]*"`
 echo $e
 printf "%-9s: " "got"
-g=`cat test.txt | grep -m 1 "move:" | grep -o "[0-9][0-9]*"`
+g=`cat f$i | grep "^L" | wc -l`
 echo $g
+if ((g > e)); then
+let h++
+fi
 if ((g > e + 2)); then
 let c++
 fi
 if ((g > e + 10)); then
 let r++
 fi
+echo $i
 sleep 1
-echo
 done
-echo "nbr of too high : $c/100"
-echo "nbr of way too high : $r/100"
+echo "nbr of high : $h/$1"
+echo "nbr of too high : $c/$1"
+echo "nbr of way too high : $r/$1"
